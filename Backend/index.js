02e959a41ee34da9f1cf431782c4c84bcf0f4ebe
rohit vankhade <rohithvankhade@gmail.com>
2026-01-31 16:37:43 +0530

    Fix signup error handling and improve validation feedback

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,14 @@ import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import authRouter from "./routes/auth.Routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 let port=process.env.PORT;
 app.use("/api/auth",authRouter)
 app.get("/",(req,res)=>{
